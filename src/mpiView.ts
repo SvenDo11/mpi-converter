332f3ea54f163dd836d72ac28c93298325c3d675
SvenDo11 <sven.donnerhak@live.com>
2023-07-13 18:09:51 +0200

    Added mpiView

---
+++ src/mpiView.ts
@@ -0,0 +1,11 @@
+import { Position, TreeView, TreeItem, TreeDataProvider } from "vscode";
+/*
+class MPIStatement extends TreeItem {
+
+    constructor(name: string, position: Position) {
+        super();
+        this.label = name;
+        this.position = position;
+    }
+}
+*/
