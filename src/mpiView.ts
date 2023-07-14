09730a1cd7556c961e61335d465c19833931dddf
SvenDo11 <sven.donnerhak@live.com>
2023-07-14 16:55:58 +0200

    Added treeview

--- src/mpiView.ts
+++ src/mpiView.ts
@@ -1,11 +1,34 @@
-import { Position, TreeView, TreeItem, TreeDataProvider } from "vscode";
-/*
-class MPIStatement extends TreeItem {
+import {
+    Position,
+    TreeView,
+    TreeItem,
+    TreeDataProvider,
+    ProviderResult,
+    window,
+} from "vscode";
 
-    constructor(name: string, position: Position) {
-        super();
-        this.label = name;
-        this.position = position;
+class MPIStatementProvider implements TreeDataProvider<MPIStatementItem> {
+    getTreeItem(
+        element: MPIStatementItem
+    ): MPIStatementItem | Thenable<MPIStatementItem> {
+        return element;
+    }
+
+    getChildren(element?: any): ProviderResult<MPIStatementItem[]> {
+        if (element) {
+            return Promise.resolve(this.getStatements());
+        } else {
+            return Promise.resolve([]);
+        }
+    }
+
+    getStatements(): MPIStatementItem[] {
+        return [new MPIStatementItem("", new Position(0, 0))];
     }
 }
-*/
+
+class MPIStatementItem extends TreeItem {
+    constructor(public readonly label: string, private pos: Position) {
+        super(label);
+    }
+}
